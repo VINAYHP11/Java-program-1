1240e1a955fff58092c0ef78844ebd49462fc9c4
Metikarthik <karthikmeti17@gmail.com>
2025-11-13 11:27:02 -0500

    Delete java model 1 directory

--- java model 1/Hello_Java.java
+++
@@ -1,5 +0,0 @@
-public class Hello_Java {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
--- java model 1/SwapNumbers.java
+++
@@ -1,9 +0,0 @@
-public class SwapNumbers {
-    public static void main(String[] args) {
-        int a = 5, b = 10;
-        a = a + b;
-        b = a - b;
-        a = a - b;
-        System.out.println("a = " + a + ", b = " + b);
-    }
-}
